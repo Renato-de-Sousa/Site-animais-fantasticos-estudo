a2c047c5867a3a6cffa85981a96557a36206cf50
Renato <renatopaulo646@gmail.com>
2022-08-07 13:33:17 -0300

    scroll suave transformado em classe

--- js/js.js
+++ js/js.js
@@ -1,25 +1,28 @@
 import tabnav from "./modulo/tabNav.js";
-import init from "./modulo/scrollSuave.js";
+import ScrollSuave from './modulo/scrollSuave.js';
 import initScroll from "./modulo/animate.js";
 import acord from "./modulo/acord.js";
 import initModal from "./modulo/modal.js";
-import initTool from "./modulo/tooltip.js"
-import initDrop from "./modulo/drop.js"
-import initMenu from "./modulo/menuMb.js"
-import initData from "./modulo/horario.js"
-import initAnimaisApi from "./modulo/initApi.js"
-import initBit from "./modulo/bitcoin.js"
+import initTool from "./modulo/tooltip.js";
+import initDrop from "./modulo/drop.js";
+import initMenu from "./modulo/menuMb.js";
+import initData from "./modulo/horario.js";
+import initAnimaisApi from "./modulo/initApi.js";
+import initBit from "./modulo/bitcoin.js";
 
-tabnav()
-init()
-initScroll()
-acord()
-initModal()
-initTool()
-initDrop()
-initMenu()
-initData()
-initAnimaisApi()
-initBit()
+const scrollSuaveAt = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
+scrollSuaveAt.init();
+
+tabnav();
+
+initScroll();
+acord();
+initModal();
+initTool();
+initDrop();
+initMenu();
+initData();
+initAnimaisApi();
+initBit();
 
 
--- js/modulo/scrollSuave.js
+++ js/modulo/scrollSuave.js
@@ -1,20 +1,34 @@
-export default function init() {
-  const linkInternos = document.querySelectorAll('[data-menu="suave"] a[href^="#"]');
+export default class ScrollSuave {
+  constructor(link, options) {
+    this.linkInternos = document.querySelectorAll(link);
+    if (options === undefined) {
+      this.options = {
+        behavior: 'smooth',
+        block: 'start',
+      };
+    } else {
+      this.options = options;
+    }
+    this.initSo = this.initSo.bind(this);
+  }
 
-  function initSo(event) {
+  initSo(event) {
     event.preventDefault();
     const href = event.currentTarget.getAttribute('href');
     const endereco = document.querySelector(href);
 
-    endereco.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
+    endereco.scrollIntoView(this.options);
+  }
+
+  addEventlink() {
+    this.linkInternos.forEach((item) => {
+      item.addEventListener('click', this.initSo);
     });
   }
 
-  if (linkInternos.length) {
-    linkInternos.forEach((item) => {
-      item.addEventListener('click', initSo);
-    });
+  init() {
+    if (this.linkInternos.length) {
+      this.addEventlink();
+    }
   }
 }
