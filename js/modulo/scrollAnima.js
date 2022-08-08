5ce1be5151268ee4cf1e2b86a8ecfb0bf283f0ec
Renato <renatopaulo646@gmail.com>
2022-08-08 16:52:43 -0300

    voltar depois para conferir

--- js/modulo/scrollAnima.js
+++ js/modulo/scrollAnima.js
@@ -2,23 +2,39 @@ export default class ScrollAnima {
   constructor(sections) {
     this.containers = document.querySelectorAll(sections);
     this.metadeDaTela = window.innerHeight * 0.7;
-    this.animate = this.animate.bind(this);
+    this.checkDistance = this.checkDistance.bind(this);
   }
 
-  animate() {
-    this.containers.forEach((item) => {
-      const distaciaEle = item.getBoundingClientRect().top;
-      const test = distaciaEle - this.metadeDaTela < 0;
-      if (test) {
-        item.classList.add('ativar');
-      } else if (item.classList.contains('ativar')) {
-        item.classList.remove('ativar');
+  getDistance() {
+    this.distance = [...this.containers].map((section) => {
+      const offSet = section.offSetTop;
+      return {
+        element: section,
+        offSet: Math.floor(offSet - this.metadeDaTela),
+      };
+    });
+  }
+
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offSet) {
+        item.element.classList.add('ativar');
+      } else if (item.element.classList.contains('ativar')) {
+        item.element.classList.remove('ativar');
       }
     });
   }
 
   init() {
-    this.animate();
-    window.addEventListener('scroll', this.animate);
+    if (this.containers.length) {
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener('scroll', this.checkDistance);
+    }
+    return this;
+  }
+
+  stop() {
+    window.removeEventListener('scroll', this.checkDistance);
   }
 }
